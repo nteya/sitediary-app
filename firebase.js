26bd1ce765e9b25cbd6fee4b173f4ca08803355a
Nteya <nteyantsika@gmail.com>
2026-05-21 07:56:45 +0200

    Updated project

--- firebase.js
+++ firebase.js
@@ -1,20 +1,21 @@
 // firebase.js
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// 🔴 Replace this with YOUR Firebase config from the Firebase console
 const firebaseConfig = {
   apiKey: "",
   authDomain: "varsity-majisty.firebaseapp.com",
-  databaseURL: "https://varsity-majisty-default-rtdb.firebaseio.com",
   projectId: "varsity-majisty",
   storageBucket: "varsity-majisty.firebasestorage.app",
   messagingSenderId: "438882953423",
   appId: "1:438882953423:web:4b8c282d7719d3a4e36fba",
-  measurementId: "G-QLBK1QFT74"
+  measurementId: "G-QLBK1QFT74",
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
-export { db };
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
